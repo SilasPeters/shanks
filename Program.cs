5bb73d0e250d1e65bfcbbbd3b837d5852093b076
Silas Peters <silaspeters03@gmail.com>
2023-10-01 14:18:56 +0200

    [REWORK/ADD] Added edge-case handling

--- Program.cs
+++ Program.cs
@@ -5,61 +5,65 @@ using System.Numerics;
 
 namespace Shanks
 {
-	static class Program
+	public static class Program
 	{
-		private static BigInteger p;
-		private static BigInteger g;
-		private static BigInteger q;
-		private static int        k;
-		private static BigInteger w;
-		private static BigInteger h;
+		private static BigInteger modulus;
+		private static BigInteger @base;
+		private static BigInteger order;
+		private static int        numberOfKeys;
+		private static BigInteger bigStepSize;
+		private static BigInteger bigStepValue;
 
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
-			 p = BigInteger.Parse(Console.ReadLine()); // Max 318 numbers, modulus
-			 g = BigInteger.Parse(Console.ReadLine()); // Max 318 numbers, base
-			 q = BigInteger.Parse(Console.ReadLine()); // Max 60 numbers, ord
-			 k = int.Parse(Console.ReadLine());        // Max 6 numbers, amount of numbers to test
-			 w = FastSquareRoot(q / k);
-			 h = BigInteger.ModPow(g, w, p);
+			modulus      = BigInteger.Parse(Console.ReadLine()); // Max 318 numbers, modulus
+			@base        = BigInteger.Parse(Console.ReadLine()); // Max 318 numbers, base
+			order        = BigInteger.Parse(Console.ReadLine()); // Max 60 numbers, order
+			numberOfKeys = int.Parse(Console.ReadLine());        // Max 6 numbers, amount of keys to crack
+			bigStepSize  = FastSquareRoot(order / numberOfKeys);
+			bigStepValue = BigInteger.ModPow(@base, bigStepSize, modulus);
 
-			Dictionary<BigInteger, BigInteger> bosjesboek = CreateBosjesBoek();
+			Dictionary<BigInteger, BigInteger> index = CreateIndex();
 
-			Span<BigInteger> xs = new BigInteger[k];
-			for (int i = 0; i < k; i++)
-				xs[i] = int.Parse(Console.ReadLine().TakeWhile(x => x != ' ').ToArray()); // Can contain a dirty suffix
+			Span<BigInteger> keys = new BigInteger[numberOfKeys];
+			for (int i = 0; i < numberOfKeys; i++)
+				keys[i] = int.Parse(Console.ReadLine().TakeWhile(c => c != ' ').ToArray());
 
-			for (int i = 0; i < k; i++)
+			for (int i = 0; i < numberOfKeys; i++)
 			{
-				BigInteger result = DiscreteLog(xs[i], ref bosjesboek);
-				Console.WriteLine(result == -1 ? "geen macht" : result.ToString());
+				BigInteger result = DiscreteLog(keys[i], ref index);
+				Console.WriteLine(result != -1 ? result.ToString() : "geen macht");
 			}
 		}
 
 		static BigInteger FastSquareRoot(BigInteger bigInteger)
 		{
 			long n = bigInteger.GetBitLength();
-			return bigInteger >> (int)(n / 2);
+			return bigInteger >> (int)(n / 2); // TODO using int might be a problem
 		}
 
-		static Dictionary<BigInteger, BigInteger> CreateBosjesBoek()
+		static Dictionary<BigInteger, BigInteger> CreateIndex()
 		{
-			Dictionary<BigInteger, BigInteger> bosjesboek = new Dictionary<BigInteger, BigInteger>();
+			Dictionary<BigInteger, BigInteger> index = new Dictionary<BigInteger, BigInteger>();
 
+			index.Add(1, 0);
 			BigInteger power = 1;
-			bosjesboek.Add(1, 0);
-			BigInteger aantalHopmanStappen = q / w;
-			for (BigInteger i = 1; i < aantalHopmanStappen + 1; i++)
-				bosjesboek.Add(power = power * h % p, i * w);
+			BigInteger bigSteps = order / bigStepSize;
+			for (BigInteger i = 1; i < bigSteps + 1; i++)
+				index.Add(power = power * bigStepValue % modulus, i * bigStepSize);
 
-			return bosjesboek;
+			return index;
 		}
 
-		static BigInteger DiscreteLog(BigInteger x, ref Dictionary<BigInteger, BigInteger> bosjesboek)
+		static BigInteger DiscreteLog(BigInteger x, ref Dictionary<BigInteger, BigInteger> index)
 		{
-			for (BigInteger i = 0; i < w; i++, x = x * g % p)
-				if (bosjesboek.TryGetValue(x, out BigInteger macht))
-					return macht - i;
+			for (BigInteger smallSteps = 0; smallSteps < bigStepSize; smallSteps++, x = x * @base % modulus)
+				if (index.TryGetValue(x, out BigInteger exponent)) {
+					var result = exponent - smallSteps;
+					return result >= 0 ? result : order - smallSteps;
+					// When the result is negative, this means we found the exponent 0 in the index. Thus, to find
+					// the discrete log, we need to subtract the smallSteps from the order of the group instead of 0.
+				}
 			return -1;
 		}
 	}
