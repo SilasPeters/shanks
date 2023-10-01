011c90c73c4ab8d9489342c147d88f35c3f349bc
Silas Peters <silaspeters03@gmail.com>
2023-10-01 16:33:29 +0200

    [REWORK] Use more refs now

--- Program.cs
+++ Program.cs
@@ -7,38 +7,40 @@ namespace Shanks
 {
 	public static class Program
 	{
-		private static BigInteger modulus;
-		private static BigInteger @base;
-		private static BigInteger order;
-		private static int        numberOfKeys;
-		private static BigInteger bigStepSize;
+		private static BigInteger modulus;      // Max 318 numbers
+		private static BigInteger @base;        // Max 318 numbers
+		private static BigInteger order;        // Max 60 numbers
+		private static int        numberOfKeys; // Max 6 numbers
+		private static BigInteger bigStepExponent;
 		private static BigInteger bigStepValue;
 
 		public static void Main(string[] args)
 		{
 			char[] legalCharacters = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-			modulus      = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray()); // Max 318 numbers, modulus
-			@base        = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray()); // Max 318 numbers, base
-			order        = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray()); // Max 60 numbers, order
-			numberOfKeys = int.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray());        // Max 6 numbers, amount of keys to crack
-			bigStepSize  = FastSquareRoot(order / numberOfKeys);
-			bigStepValue = BigInteger.ModPow(@base, bigStepSize, modulus);
+			modulus         = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray());
+			@base           = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray());
+			order           = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray());
+			numberOfKeys    = int.Parse(Console.ReadLine()       .TakeWhile(c => legalCharacters.Contains(c)).ToArray());
+			bigStepExponent = FastSquareRoot(ref order, ref numberOfKeys);
+			bigStepValue    = BigInteger.ModPow(@base, bigStepExponent, modulus);
 
+			// Key: power, Value: exponent
 			Dictionary<BigInteger, BigInteger> index = CreateIndex();
 
-			Span<BigInteger> keys = new BigInteger[numberOfKeys];
+			BigInteger[] keys = new BigInteger[numberOfKeys];
 			for (int i = 0; i < numberOfKeys; i++)
-				keys[i] = BigInteger.Parse(Console.ReadLine().TakeWhile(c => legalCharacters.Contains(c)).ToArray());
+				keys[i] = BigInteger.Parse(Console.ReadLine().TakeWhile(c => c != ' ').ToArray());
 
 			for (int i = 0; i < numberOfKeys; i++)
 			{
-				BigInteger result = DiscreteLog(keys[i], ref index);
-				Console.WriteLine(result != -1 ? result.ToString() : "geen macht");
+				DiscreteLog(ref keys[i], ref index);
+				Console.WriteLine(keys[i] != -1 ? keys[i].ToString() : "geen macht");
 			}
 		}
 
-		static BigInteger FastSquareRoot(BigInteger bigInteger)
+		static BigInteger FastSquareRoot(ref BigInteger orderRef, ref int numberOfKeysRef)
 		{
+			BigInteger bigInteger = orderRef / numberOfKeysRef;
 			int        bits = 0;
 			BigInteger i    = 1;
 			while (i << bits < bigInteger) { bits += 1; }
@@ -51,23 +53,23 @@ namespace Shanks
 
 			index.Add(1, 0);
 			BigInteger power = 1;
-			BigInteger bigSteps = order / bigStepSize;
+			BigInteger bigSteps = order / bigStepExponent;
 			for (BigInteger i = 1; i < bigSteps + 1; i++)
-				index.Add(power = power * bigStepValue % modulus, i * bigStepSize);
+				index.Add(power = power * bigStepValue % modulus, i * bigStepExponent);
 
 			return index;
 		}
 
-		static BigInteger DiscreteLog(BigInteger x, ref Dictionary<BigInteger, BigInteger> index)
+		static void DiscreteLog(ref BigInteger x, ref Dictionary<BigInteger, BigInteger> index)
 		{
-			for (BigInteger smallSteps = 0; smallSteps < bigStepSize; smallSteps++, x = x * @base % modulus)
+			for (BigInteger smallSteps = 0; smallSteps < bigStepExponent; smallSteps++, x = x * @base % modulus)
 				if (index.TryGetValue(x, out BigInteger exponent)) {
 					var result = exponent - smallSteps;
-					return result >= 0 ? result : order - smallSteps;
+					x = result >= 0 ? result : order - smallSteps;
 					// When the result is negative, this means we found the exponent 0 in the index. Thus, to find
 					// the discrete log, we need to subtract the smallSteps from the order of the group instead of 0.
 				}
-			return -1;
+			x = -1;
 		}
 	}
 }
